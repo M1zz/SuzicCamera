d33a8466a4b4307030221a04622e0d18405ec063
hyunho.lee <mizzking75@gmail.com>
2021-03-16 16:38:05 +0900

    Add flash mode

--- SuzicCamera/Custom Views/Views/CameraToolbarView.swift
+++ SuzicCamera/Custom Views/Views/CameraToolbarView.swift
@@ -6,17 +6,17 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraToolbarView: UIView {
 
     @IBOutlet weak var flashButton: UIButton!
-
     @IBOutlet weak var gridButton: UIButton!
-
     @IBOutlet weak var ratioButton: UIButton!
-
     @IBOutlet weak var settingButton: UIButton!
 
+    private var flashToggle: Bool = false
+    
     override func awakeFromNib() {
         flashButton.layer.cornerRadius = 0.5 * flashButton.bounds.size.width
         gridButton.layer.cornerRadius = 0.5 * gridButton.bounds.size.width
@@ -24,4 +24,42 @@ class CameraToolbarView: UIView {
         settingButton.layer.cornerRadius = 0.5 * settingButton.bounds.size.width
         
     }
+    
+    
+    @IBAction func didTapFlashButton(_ sender: Any) {
+        flashToggle.toggle()
+        // chageFlashStatusIcon()
+        print("Flash \(flashToggle)")
+    }
+    
+    static func asdf() {
+        print("asdf")
+    }
+    
+    open func getFlashMode() -> AVCaptureDevice.FlashMode {
+        var valueOfAVCaptureFlashMode: AVCaptureDevice.FlashMode = .off
+        
+        switch flashToggle {
+        case false:
+            valueOfAVCaptureFlashMode = .off
+        case true:
+            valueOfAVCaptureFlashMode = .on
+        }
+        return valueOfAVCaptureFlashMode
+    }
+
+    
+    @IBAction func didTapGridButton(_ sender: Any) {
+        print("grid On/Off")
+    }
+    
+    @IBAction func didTapChangeRatioButton(_ sender: Any) {
+        print("Ratio On/Off")
+    }
+    
+    
+    @IBAction func didTapSettingButton(_ sender: Any) {
+        print("Get Setting Page")
+    }
+    
 }
