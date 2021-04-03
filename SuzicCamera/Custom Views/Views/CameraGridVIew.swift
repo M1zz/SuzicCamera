bcd6a2e636c9135f1bdd1288ac91a35c5c287d6e
hyunho.lee <mizzking75@gmail.com>
2021-04-03 22:49:29 +0900

    Add gridView

---
+++ SuzicCamera/Custom Views/Views/CameraGridVIew.swift
@@ -0,0 +1,14 @@
+//
+//  CameraGridVIew.swift
+//  SuzicCamera
+//
+//  Created by 이현호 on 2021/04/03.
+//
+
+import UIKit
+
+
+class CameraGridVIew: UIView {
+    @IBOutlet weak var horizontalLeftGridView: UIImageView!
+    @IBOutlet weak var horizontalRightGridView: UIImageView!
+}
